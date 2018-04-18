7634cef0981f07fdcc93df8cdcfc5aef0d4df6e3
HamBoy1 <31289226+HamBoy1@users.noreply.github.com>
2018-04-17 20:36:31 -0500

    converts any letters in upp to numbers

--- main.rb
+++ main.rb
@@ -5,15 +5,27 @@ upp.pop
 
 options = ['A','B','C','D','E','F','1','2','3','4','5','6','7','8','9']
 extras = (upp - options)
-
 def hexCheck(upp, extras)
   if extras.length != 0
     puts "invalid UPP"
   elsif (upp.length) != 6
     puts "invalid UPP"
   else
-    puts "okay"
+    puts "valid UPP"
   end
 end
 
-hexCheck(upp, extras)
+flips = ['A','B','C','D','E','F']
+def changeChange(upp, flips)
+  space = 0
+  for val in upp
+    if val == "A" or val == "B" or val == "C" or val == "D" or val == "E" or val == "F"
+      upp[space] = flips.index(val) + 10
+    end
+    space = space +1
+  end
+  print upp
+end
+
+hexCheck(upp, extras) #checks that upp is valid
+changeChange(upp, flips) #changes any letters to numbers
