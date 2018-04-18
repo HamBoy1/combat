4f444378f646ef644c57f9eca7539af23d60b31b
HamBoy1 <31289226+HamBoy1@users.noreply.github.com>
2018-04-17 21:09:55 -0500

    checks requirements for weapon

--- main.rb
+++ main.rb
@@ -29,3 +29,42 @@ end
 
 hexCheck(upp, extras) #checks that upp is valid
 changeChange(upp, flips) #changes any letters to numbers
+
+
+################################################################################
+
+puts "enter the weapon"
+weapon = gets.upcase
+weapon = weapon.chomp
+
+weaponOptions = ['HANDS','CLAWS','TEETH','HORNS','HOOVES','STINGER','THRASHER','CLUB','DAGGER','BLADE','FOIL','CUTLASS','SWORD','BROADSWORD','BAYONET','SPEAR','HALBERD','PIKE','CUDGEL']
+requiredCheck = [6,0,0,0,0,0,0,5,4,5,5,7,6,8,5,5,6,7,5]
+advantageousCheck = [9,0,0,0,0,0,0,8,8,9,10,11,10,12,9,8,10,10,8]
+advantageousBlow = [1,0,0,0,0,0,0,2,2,1,1,2,1,2,2,2,2,2,2]
+weakenedBlow = [2,0,0,0,0,0,0,1,2,2,2,4,3,4,2,3,3,3,1]
+
+
+if  !weaponOptions.include?(weapon)
+  puts "invalid weapon"
+else
+  puts "valid weapon"
+end
+
+
+puts "enter strength"
+strength = gets
+
+spot = weaponOptions.index(weapon)
+blowDM = 0
+
+if (strength.to_i >= advantageousCheck.fetch(spot))
+  puts "extra strong ;)"
+  blowDM = advantageousBlow.fetch(spot)
+elsif (strength.to_i < requiredCheck.fetch(spot))
+  puts "too low... :("
+  blowDM = 0 - weakenedBlow.fetch(spot)
+else
+  puts "okie dokie"
+end
+
+puts blowDM
